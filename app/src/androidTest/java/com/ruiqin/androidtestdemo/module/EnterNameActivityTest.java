b757cf9dbda67a3ba6eb6b34e0cc4ccea04921b0
ruiqin.shen <ruiiqn.shen@ecaray.com>
2017-12-15 19:18:03 +0800

    init

---
+++ app/src/androidTest/java/com/ruiqin/androidtestdemo/module/EnterNameActivityTest.java
@@ -0,0 +1,20 @@
+package com.ruiqin.androidtestdemo.module;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 类描述:
+ *
+ * @author : ruiqin.shen
+ * @date : 2017/12/15
+ */
+@RunWith(AndroidJUnit4.class)
+public class EnterNameActivityTest {
+
+}
---
+++ app/src/main/java/com/ruiqin/androidtestdemo/module/DisplayNameActivity.java
@@ -0,0 +1,21 @@
+package com.ruiqin.androidtestdemo.module;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import com.ruiqin.androidtestdemo.R;
+
+
+public class DisplayNameActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_name);
+
+        String text = getIntent().getStringExtra(EnterNameActivity.NAME);
+        TextView greetingTextView = (TextView) findViewById(R.id.greeting_message);
+        greetingTextView.setText("Hello " + text + "!");
+    }
+}
